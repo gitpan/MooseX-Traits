cc6859f4101d7e77ad399ab4d5dfbb5dbaf53fa0
Florian Ragwitz <rafl@debian.org>
2010-05-13 12:33:05 +0000

    Import of FLORA/MooseX-Traits-0.11 from CPAN.
    gitpan-cpan-distribution: MooseX-Traits
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         FLORA/MooseX-Traits-0.11.tar.gz
    gitpan-cpan-author:       FLORA
    gitpan-cpan-maturity:     released

--- lib/MooseX/Traits.pm
+++ lib/MooseX/Traits.pm
@@ -8,7 +8,7 @@ use warnings::register;
 
 use namespace::autoclean;
 
-our $VERSION   = '0.10';
+our $VERSION   = '0.11';
 our $AUTHORITY = 'id:JROCKWAY';
 
 has '_trait_namespace' => (
@@ -126,7 +126,7 @@ with the desired roles applied, or apply roles to the instance
 one-by-one, he can just create a new class from yours with
 C<with_traits>, and then instantiate that.
 
-There is also C<new_with_traits>, which exists for compatability
+There is also C<new_with_traits>, which exists for compatibility
 reasons.  It accepts a C<traits> parameter, creates a new class with
 those traits, and then insantiates it.
 
